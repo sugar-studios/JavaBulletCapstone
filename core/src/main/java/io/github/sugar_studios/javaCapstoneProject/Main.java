4656063f1820a7c717f6160f64f7992d62597ca7
Calkins <abcalkins8723@gmail.com>
2026-05-10 22:50:19 -0700

    Reflection

--- core/src/main/java/io/github/sugar_studios/javaCapstoneProject/Main.java
+++ core/src/main/java/io/github/sugar_studios/javaCapstoneProject/Main.java
@@ -64,15 +64,17 @@ public class Main extends Game {
  * My project is a bullet Hell demo. I implmented a probablity queue, a input  hasmap, enemies that follow a script
  * of movement and shooting. A wave system.
  * 2) Which concepts from CSC205 did you utilize in your project?
- * A lot of hashmaps, arrays, and queues. These are nested inside each other to make the core of the wave system
+ * A lot of hashmaps, arrays, and queues. I also used this to make my own "custom" data structture, that being the Probablity Queue. These are nested inside each other to make the core of the wave system
  * 3) What challenges did you encounter, and how did you overcome them?
  *  I had a really hard time figuring out how to get eneimes to work, bullets were easy but thinking of how to make enmies work and fly around.
  *  animations were also really hard (not the art), getting the code to work involved a lot of fitanguiling.
  *  I werid struggle was that on 5/3 I reinstalled windows on my PC, which normally would be fine but I realized I had only ported 1 enemy asset into
- *  my game... which was a challenge because I lost hours of sprite working... I overcame this by pretendeding that didnt happen.
+ *  my game... which was a challenge because I lost hours of sprite working... I overcame this by pretendeding that didnt happen. I also struggled with audio for hours, couldnt get it to work.
+ * I still cant get JP text to work >:(
  * 4) What are you most proud of in your project?
  *   None of it really, it was rushed and it shows. If I hadn't had waited until the week it was assigned I probably would've finished my vision.
- *   I had already planned out this before hand but didnt start working on it till the last second.
+ *   I had already planned out this beforehand but didn't start working on it till the last second. But if anything I really am proud of the cutscene art and the wave system
+ *    I think it was an elogant solution.
  * 5) If you had more time, what would you add or improve?
  *  Everything.
  */
